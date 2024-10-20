4cc256ec2a907bdedb33b08df44c5b1254fc039c
Huaish <zsisc.huaish@gmail.com>
2024-10-20 11:15:19 +0800

    feat(hw2): implement task3 and add save image function in utils.py

--- hw2/pyramid_image.py
+++ hw2/pyramid_image.py
@@ -1,41 +1,29 @@
 import cv2
 import numpy as np
-from matplotlib import pyplot as plt
 from filter import gaussian_filter
+from utils import save_image_grid
 
 # Load the input image
 img = cv2.imread('data/task1and2_hybrid_pyramid/1_motorcycle.bmp', cv2.IMREAD_GRAYSCALE)
 
-
 # List to store pyramid images
 pyramid_images = [img]
 fft_images = [np.fft.fft2(img)]
-
-# Generate the Gaussian pyramid (e.g., 4 levels)
+num_levels = 4
 current_image = img
-for _ in range(4):
-    smoothed = gaussian_filter(current_image, cutoff_frequency=20)
+
+for _ in range(num_levels):
+    smoothed = gaussian_filter(current_image, cutoff_frequency=20, low_pass=False)
     downscaled = smoothed[::2, ::2]
     if downscaled.shape[0] < 2 or downscaled.shape[1] < 2:
         break
     pyramid_images.append(downscaled)
     current_image = downscaled
     fft_images.append(np.fft.fft2(downscaled))
-
-# Use matplotlib to plot all the pyramid levels
+    
+# Save the image grid
 num_levels = len(pyramid_images)
-fig, axes = plt.subplots(2, num_levels, figsize=(15, 5))
+images = [cv2.cvtColor(pyramid_image, cv2.COLOR_BGR2RGB) for pyramid_image in pyramid_images]
+images += [np.log(1 + np.abs(np.fft.fftshift(fft_image))) for fft_image in fft_images]
 
-for i in range(num_levels):
-    axes[0, i].imshow(cv2.cvtColor(pyramid_images[i], cv2.COLOR_BGR2RGB), cmap='gray')
-    # axes[0, i].set_title(f'Level {i}')
-    axes[0, i].axis('off')
-
-    # Plot the Fourier transform of the image
-    fft_image = np.fft.fftshift(fft_images[i])
-    axes[1, i].imshow(np.log(1 + np.abs(fft_image)))
-    # axes[1, i].set_title(f'FFT Level {i}')
-    axes[1, i].axis('off')
-
-plt.tight_layout()
-plt.show()
+save_image_grid(images, [f'Level {i%num_levels+1}' for i in range(num_levels*2)], 2, num_levels, output_path='output/task2_gaussian_pyramid.jpg')
